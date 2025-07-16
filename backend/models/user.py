b435d4dcca191747b3c80ed323f90c21904a40e9
amirr-k <damianwritescode@gmail.com>
2025-07-15 22:15:44 -0700

    Setitng up user information

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,1 +1,20 @@
-# User SQLAlchemy model (id, email, password) 
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from database import base
+
+class User(base):
+    __tablename__ = 'users'
+    
+    #Basic User Information
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False) #First and Last Name, Self Def.
+    college = Column(String, nullable=False) #Which UC Campus (Restricted)
+    school = Column(String, nullable=False) #For UCSD, College System, for others, L&S, Engineering, etc
+    year = Column(Integer, nullable=False) #'2025, '2026, etc
+    gender = Column(String, nullable=False)# Male, Female, Other (Specified)
+    major = Column(String, nullable=False) #Major -- Self Def.
+    created_at = Column(DateTime) 
+    
+
+    
