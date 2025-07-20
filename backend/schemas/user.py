d946094b78e2178fc361554962d9491e4cb6f2f3
amirr-k <damianwritescode@gmail.com>
2025-07-19 19:01:34 -0700

    Implementing User Schema

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -1,1 +1,21 @@
-# Pydantic schemas (SignupRequest, LoginRequest) 
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class UserBase(BaseModel):
+    email: str
+    name: str
+    college: str
+    school: str
+    year: int
+    gender: str
+    major: str
+    profile_pic_url: Optional[str] = None
+    bio: Optional[str] = None
+    interests: Optional[str] = None
+
+class UserResponse(UserBase):
+    id: int
+    created_at: Optional[datetime]
+    class Config:
+        orm_mode = True
