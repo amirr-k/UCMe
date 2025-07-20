4d393066a9db397caae2ec389ea15e00d3c62a27
amirr-k <damianwritescode@gmail.com>
2025-07-19 20:09:53 -0700

    Implementing error handling for db

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,14 @@
 # SQLAlchemy DB connection and session 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 databaseUrl = os.getenv('dburl')
+if not databaseUrl:
+    raise RuntimeError("Failed to retrieve database from .env file")
 
 engine = create_engine(databaseUrl)
-localSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+localSession = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
 base = declarative_base()
-
