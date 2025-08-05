db0e68b7abcd02eaa657cb40edc7c9f8afa6d63f
Amir <damianwritescode@gmail.com>
2025-08-04 19:11:31 -0700

    Adding main hook function

---
+++ src/hooks/useConversation.js
@@ -0,0 +1,7 @@
+import { useState, useEffect, useCallback } from 'react';
+import { getConversation, sendMessage, markConversationAsRead } from '../services/messageService';
+
+export default function Conversations(conversationId) {
+    
+
+}
