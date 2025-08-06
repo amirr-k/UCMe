2097579a77cfcb1f91c68a8d0cecea03661f2221
Amir <damianwritescode@gmail.com>
2025-08-05 23:25:13 -0700

    Added new conv. component

---
+++ src/components/messaging/NewConversation.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createConversation } from '../../services/messageService';
+import '../../styles/messaging/NewConversation.css';
