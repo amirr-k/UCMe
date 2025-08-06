a057b216b7439d16ee30611d659455e8a4137d49
Amir <damianwritescode@gmail.com>
2025-08-05 23:31:55 -0700

    Worked on newconv. component, added some css to conv.list

--- src/components/messaging/NewConversation.js
+++ src/components/messaging/NewConversation.js
@@ -2,3 +2,30 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createConversation } from '../../services/messageService';
 import '../../styles/messaging/NewConversation.css';
+
+function NewConversation({ userId, userName, userImage, onConversationCreated }) {
+    const navigate = useNavigate();
+    const [creating, setCreating] = React.useState(false);
+    const [error, setError] = React.useState(null);
+    
+    const handleStartConversation = async () => {
+        setCreating(true);
+        setError(null);
+
+        try {
+            const conversation = await createConversation(userId);
+            if (onConversationCreated) {
+                onConversationCreated(conversation);
+            }
+            else {
+                navigate('/messages/${conversation.id}');
+            }
+        catch(error){
+            setError(error);
+            console.error('Error starting conversation:', error)
+        }
+        finally{
+            setCreating(false);
+        }
+    };
+    
